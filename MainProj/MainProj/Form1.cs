14f486344f58c6c05fcf52ffd1208b57bc8c78a8
VlodyaMakovs <vova.makovskiy.a98@gmail.com>
2022-09-27 12:08:30 +0300

    Create Project
    Create first From
    Create class UserData

---
+++ MainProj/MainProj/Form1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MainProj
+{
+    public partial class Form1 : Form
+    {
+
+        UserData userData;
+
+        public Form1()
+        {
+            InitializeComponent();
+            userData = new UserData(-1, null, null, null, -1);
+        }
+
+
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+    }
+}
---
+++ MainProj/MainProj/UserData.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MainProj
+{
+    internal class UserData
+    {
+        private int id;
+        private string name;
+        private string email;
+        private string password;
+        private int role;
+
+        public UserData(int id, string name, string email, string password, int role)
+        {
+            this.Id = id;
+            this.Name = name;
+            this.Email = email;
+            this.Password = password;
+            this.Role = role;  // 1-admin 2-moder 3-user 4-banUser
+        }
+
+
+        public int Id { get => id; set => id = value; }
+        public string Name { get => name; set => name = value; }
+        public string Email { get => email; set => email = value; }
+        public string Password { get => password; set => password = value; }
+        public int Role { get => role; set => role = value; }
+    }
+}
